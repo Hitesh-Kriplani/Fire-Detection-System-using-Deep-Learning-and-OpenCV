172489796ac62e23a73d74563c0349650a6b4155
Hitesh-Kriplani <hiteshkriplani00@gmail.com>
2022-09-09 16:24:15 +0530

    Changed alarm mimic logic.

--- live.py
+++ live.py
@@ -3,24 +3,18 @@ import numpy as np
 from PIL import Image
 import tensorflow as tf
 from keras.preprocessing import image
-#Load the saved model
-model = tf.keras.models.load_model('./Fire_Detection_Model_V1.h5')
-video = cv2.VideoCapture(0) ##
+model = tf.keras.models.load_model('./Fire_Detection_Model.h5')
+video = cv2.VideoCapture(0)
 while True:
-        _, frame = video.read() ##
-        #Convert the captured frame into RGB
+        _, frame = video.read()
         im = Image.fromarray(frame, 'RGB')
-        #Resizing into 224x224 because we trained the model with this image size.
         im = im.resize((224,224))
         img_array = image.img_to_array(im)
         img_array = np.expand_dims(img_array, axis=0) / 255
         probabilities = model.predict(img_array)[0]
-        #Calling the predict method on model to predict 'fire' on the image
-        prediction = np.argmax(probabilities)
-        #if prediction is 0, which means there is fire in the frame.
-        if prediction == 0:
+        if probabilities[0] > 0.70:
                 frame = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
-                print(probabilities[prediction])
+                print(probabilities[0])
         cv2.imshow("Capturing", frame)
         key=cv2.waitKey(1)
         if key == ord('q'):
